438947b3f37e66ab0b3a483c17b38eb1982c7274
hongbinbao <hongbin.bao@gmail.com>
2014-05-23 14:53:05 +0800

    init

---
+++ sample/test.py
@@ -0,0 +1,48 @@
+from com.android.monkeyrunner import MonkeyRunner as runner
+from com.android.monkeyrunner import MonkeyDevice as device
+from com.bhb.verify import Verifier as vf
+
+#image save path with file name
+app_phone_screen_1 = '/home/test/Desktop/imonkeyrunner1/phone_dialer_screen_1.png'
+#image checkpoint resource path with file name
+app_phone_screen_1_checkpoint_1 = '/home/test/Desktop/imonkeyrunner1/phone_dialer_screen_1_checkpoint_1.png'
+
+#image touch-able resource path with file name
+app_phone_dialbutton_icon = '/home/test/Desktop/imonkeyrunner1/phone_dial_icon.png'
+
+DUT = runner.waitForConnection()
+
+DUT.wake()
+
+DUT.press('KEYCODE_BACK', device.DOWN_AND_UP)
+
+DUT.press('KEYCODE_BACK', device.DOWN_AND_UP)
+
+DUT.press('KEYCODE_HOME', device.DOWN_AND_UP)
+
+DUT.startActivity(action='android.intent.action.DIAL tel:10086')
+
+runner.sleep(1)
+
+DUT.takeSnapshot().writeToFile(app_phone_screen_1)
+#find(string childPicturePath, string parentPicturePath, double similarity)
+#similarity: the minimum similarity to use in a find operation. The value should be between 0 and 1
+#searches the child picture using a default minimum similarity of 0.7 if not provide in method argument.
+exist =  vf().find(app_phone_screen_1_checkpoint_1, app_phone_screen_1, 0.8)
+if exist:
+   pass
+else:
+   print '\n%s not found' % app_phone_screen_1_checkpoint_1
+
+#touch the center point coordinate of the image which exists in current screenshot
+DUT.takeSnapshot().writeToFile(app_phone_screen_1)
+#search the touch-able image and return the center point location coordinate in natural screen orientation
+point =  vf().getLocation(app_phone_dialbutton_icon, app_phone_screen_1)
+
+if point:
+    x_orientation_natural, y_orientation_natural = point
+    #touch API of MonkeyDevice
+    DUT.touch(x_orientation_natural, y_orientation_natural, device.DOWN_AND_UP)
+else:
+    print '\n%s not found' % app_phone_dialbutton_icon
+
